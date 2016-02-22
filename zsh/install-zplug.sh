9a7d4b65c55d95aea17dd0552ea8cdf57895bc89
Dale Davis <dmdavis.net@gmail.com>
2016-02-22 06:01:04 -0800

    Fixed git typo in zsh/install-zplug.sh

--- zsh/install-zplug.sh
+++ zsh/install-zplug.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -d $HOME/.zplug ]; then
-    it clone https://github.com/b4b4r07/zplug $HOME/.zplug
+    git clone https://github.com/b4b4r07/zplug $HOME/.zplug
 else
     if [ command -v zplug ]; then
         zplug update
