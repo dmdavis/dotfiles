14789623e7116006e70dc84cea2ff7fccc101457
Dale Davis <dmdavis.net@gmail.com>
2016-02-28 12:57:47 -0800

    Added brewfile command

--- homebrew/homebrew.plugin.zsh
+++ homebrew/homebrew.plugin.zsh
@@ -3,6 +3,10 @@
 export HOMEBREW_CELLAR="`brew --prefix`/Cellar"
 alias cellar="cd $HOMEBREW_CELLAR"
 
+function brewfile {
+    brew bundle --file="$DOTFILES/homebrew/brewfile"
+}
+
 # tccutil uses homebrew's Python 2.7, not pyenv's
 function tccutil {
     sudo /usr/local/bin/python /usr/local/bin/tccutil "$@"
