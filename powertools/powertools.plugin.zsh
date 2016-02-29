fff7a28faba6f0b5fdbae9b879b506a8afd90c4c
Dale Davis <dmdavis.net@gmail.com>
2016-02-28 19:06:25 -0800

    Added Node/NVM development prompt theming
    * Added nvm to brewfile
    * Added node plugin and nvm initialization to zshrc
    * Colorized plc and added node prompt support

--- powertools/powertools.plugin.zsh
+++ powertools/powertools.plugin.zsh
@@ -1,18 +1,26 @@
 #!/usr/bin/env zsh
 
 POWERLEVEL9K_MODE='awesome-patched'
+POWERLEVEL9K_NVM_FOREGROUND='black'
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
 
 # Power Level 9000 customization
 function plc {
     case "$1" in
         ruby)
-            echo "Configuring powelevel9k for ruby development"
+            echo "$FG[yellow]\uE802$FG[none]  Configuring $FG[blue]\uE0B2$BG[blue]$FG[black]Power Level $FG[bold]9000$BG[none]$FG[blue]\uE0B0$FG[none] for $FG[red]\uE847 Ruby$FG[none] development"
+            echo "Configuring $FG[blue]powelevel9k$FG[none] for $FG[red]ruby$FG[none] development"
             POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
             POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
             ;;
+        node)
+            echo "$FG[yellow]\uE802$FG[none]  Configuring $FG[blue]\uE0B2$BG[blue]$FG[black]Power Level $FG[bold]9000$BG[none]$FG[blue]\uE0B0$FG[none] for $FG[green]\u2B22 Node$FG[none] development"
+            POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir nvm vcs)
+            POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
+            ;;
         *)
-            echo "Configuring powerlevel9k with default settings"
+            echo "$FG[yellow]\uE802$FG[none]  Configuring $FG[blue]\uE0B2$BG[blue]$FG[black]Power Level $FG[bold]9000$BG[none]$FG[blue]\uE0B0$FG[none] with default settings"
+            echo "Configuring $FG[blue]powelevel9k$FG[none] with default settings"
             POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
             POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
     esac
