e8fd6fe68c044bb5f6e10e60c663b55fe4262e93
Dale Davis <dmdavis.net@gmail.com>
2016-02-28 21:13:28 -0800

    Removed left-over cruft in plc default

--- powertools/powertools.plugin.zsh
+++ powertools/powertools.plugin.zsh
@@ -20,7 +20,6 @@ function plc {
             ;;
         *)
             echo "$FG[yellow]\uE802$FG[none]  Configuring $FG[blue]\uE0B2$BG[blue]$FG[black]Power Level $FG[bold]9000$BG[none]$FG[blue]\uE0B0$FG[none] with default settings"
-            echo "Configuring $FG[blue]powelevel9k$FG[none] with default settings"
             POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
             POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status history time)
     esac
