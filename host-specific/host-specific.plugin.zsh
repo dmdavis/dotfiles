11f6cb7e50b15c8b66e9e074bd6b4d146102be60
Dale Davis <dmdavis.net@gmail.com>
2016-02-29 10:49:16 -0800

    Moved host-specific up from /zsh

--- host-specific/host-specific.plugin.zsh
+++ host-specific/host-specific.plugin.zsh
@@ -2,5 +2,5 @@
 
 if [[ -f "$HOME/.host-specific" ]]; then
     host_specific=$(<"$HOME/.host-specific")
-    source "$DOTFILES/zsh/host-specific/$host_specific"
+    source "$DOTFILES/host-specific/$host_specific"
 fi
