2613d4485d69d0a36abd9f5db9fa92e72ee77416
Dale Davis <dmdavis.net@gmail.com>
2016-03-18 09:08:31 -0700

    Misc homebrew/linuxbrew changes
    * Added dockertoolbox and karabiner to brewfile
    * Overrode some of prezto's homebrew aliases
    * Updated some work SSH shortcuts
    * Added BREWFILE_PATH variable to zshrc

--- homebrew/homebrew.plugin.zsh
+++ homebrew/homebrew.plugin.zsh
@@ -4,8 +4,13 @@ export HOMEBREW_CELLAR="`brew --prefix`/Cellar"
 alias cellar="cd $HOMEBREW_CELLAR"
 alias caskI='brew cask install --force'
 
+# Override prezto homebrew aliases
+alias brewu='brew update && brew outdated'
+alias brewU='brew update && brew upgrade --all && brew cleanup'
+alias brewv='brew upgrade --all && brew cleanup'
+
 function brewfile {
-    brew bundle --file="$DOTFILES/homebrew/brewfile"
+    brew bundle --file="$BREWFILE_PATH"
 }
 
 # tccutil uses homebrew's Python 2.7, not pyenv's
