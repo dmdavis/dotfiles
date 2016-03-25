9bde7b4c04c6617ec950779733d7c63b2eb93b87
Dale Davis <dmdavis.net@gmail.com>
2016-03-25 12:58:18 -0700

    Added pyenv-virtualenv to brewfile-linux and updated pyenv.plugin.zsh

--- pyenv/pyenv.plugin.zsh
+++ pyenv/pyenv.plugin.zsh
@@ -1,8 +1,15 @@
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-pyenv virtualenvwrapper
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-alias workon="pyenv global $1"
-alias wo="workon"
-alias lsv="pyenv versions"
-alias rmv="pyenv uninstall"
+#!/usr/bin/env zsh
+
+export PYENV_ROOT="${HOME}/.pyenv"
+if [ -d "${PYENV_ROOT}" ]; then
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv virtualenvwrapper
+    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+    alias workon="pyenv global $1"
+    alias wo="workon"
+    alias lsv="pyenv versions"
+    alias rmv="pyenv uninstall"
+fi
+export PATH
