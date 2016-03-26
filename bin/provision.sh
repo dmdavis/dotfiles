98f8e1bbf9018f7f4efdc106dd2328e8fa3b83cc
Dale Davis <dmdavis.net@gmail.com>
2016-03-25 19:18:13 -0700

    Added linuxbrew init to provision.sh

--- bin/provision.sh
+++ bin/provision.sh
@@ -13,9 +13,11 @@ if [[ "$OS" == Darwin* ]]; then
     echo "Installing/upgrading Homebrew and Cask packages"
     command -v brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew tap homebrew/bundle 2>/dev/null;
-    brew bundle --file="$DIR/../homebrew/brewfile"
+    brew bundle --file="$DIR/../homebrew/brewfile-osx"
 elif [[ "$OS" == Fedora* ]]; then
     echo "TODO: Installing/upgrading yum packages"
 else # Debian, Ubuntu, Linux Mint, and other apt-based distros
-    echo "TODO: Installing/upgrading apt packages"
+    command -v brew >/dev/null 2>&1 || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)"
+    brew tap homebrew/bundle 2>/dev/null;
+    brew bundle --file="$DIR/../homebrew/brewfile-linux"
 fi
