02f0829f44cd10df5ce696ec7a93d6d797e626de
Dale Davis <dmdavis.net@gmail.com>
2016-03-31 09:36:30 -0700

    Added local ssh plugin

---
+++ ssh/ssh.plugin.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+function ssh-to {
+	bat $1;
+	echo -e "\033]6;1;bg;red;brightness;209\a"
+	echo -e "\033]6;1;bg;green;brightness;209\a"
+	echo -e "\033]6;1;bg;blue;brightness;254\a"
+	ssh $2;
+	bat ""
+	echo -e "\033]6;1;bg;*;default\a"
+}
+
+function backtomymac {
+    ssh-to home dale@retina-pro.`echo show Setup:/Network/BackToMyMac | scutil | sed -n 's/.* : *\(.*\).$/\1/p'`
+}
+
+alias htpc='ssh-to htpc dale@htpc.local'
+
