a9e9e44b8ab9fa32b21b69832c896856bc623804
Dale Davis <dmdavis.net@gmail.com>
2016-04-01 15:10:19 -0700

    Enabled vim mode in zshrc and added new virtualbox aliases

--- virtualbox/virtualbox.plugin.zsh
+++ virtualbox/virtualbox.plugin.zsh
@@ -2,6 +2,11 @@
 
 if (( $+commands[VBoxManage] )); then
     alias vb='VBoxManage'
-    alias vbnn='VBoxManage natnetwork'
+    alias vbn='VBoxManage natnetwork'
+    alias vbns='VBoxManage natnetwork start'
+    alias vbnx='VBoxManage natnetwork stop'
+    alias vbnr='VBoxManage natnetwork remove'
+    alias vbnm='VBoxManage natnetwork manage'
+    alias vbnl='VBoxManage list natnetworks'
 fi
 
