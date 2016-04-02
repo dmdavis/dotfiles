57f48b52ea20924e54e810c8c026744eb4b97b50
Dale Davis <dmdavis.net@gmail.com>
2016-04-02 06:56:25 -0700

    Updated fzf.zsh to work under both linuxbrew and homebrew

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,20 +1,27 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if (( $+commands[fzf] )); then
+    if [[ -d "$HOME/.linuxbrew" ]]; then
+        local OPT_PATH="$HOME/.linuxbrew/opt"
+    else
+        local OPT_PATH='/usr/local/opt'
+    fi
+
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == *$OPT_PATH/fzf/bin* ]]; then
+      export PATH="$PATH:$OPT_PATH/fzf/bin"
+    fi
+
+    # Man path
+    # --------
+    if [[ ! "$MANPATH" == *$OPT_PATH/fzf/man* && -d "$OPT_PATH/fzf/man" ]]; then
+      export MANPATH="$MANPATH:$OPT_PATH/fzf/man"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "$OPT_PATH/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "$OPT_PATH/fzf/shell/key-bindings.zsh"
 fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
