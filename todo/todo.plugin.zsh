05db3a51ccb32b941aa7348cba3497496ff8b87c
Dale Davis <dmdavis.net@gmail.com>
2017-02-28 13:31:21 -0800

    Updated todo functions to display list after non-list actions

--- todo/todo.plugin.zsh
+++ todo/todo.plugin.zsh
@@ -1,2 +1,15 @@
 #!/usr/bin/env zsh
-alias t='todo.sh -d ~/.files/todo/todo.cfg'
+alias todo='todo.sh -d ~/.files/todo/todo.cfg'
+function t {
+# Default (list)
+    if [[ "$#" < 1 ]]; then
+        todo
+    # Any (l)ist command
+    elif [ "$1" =~ '^l.+$' ]; then
+        todo "$1"
+    # All other commands are followd by list
+    else
+        todo "$@"
+        todo ls
+    fi
+}
