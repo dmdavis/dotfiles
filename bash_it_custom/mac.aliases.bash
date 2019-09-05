14ed1928fa1bbfc506e4443a646b974f0bc04c28
Dale Davis <dmdavis.net@github.com>
2019-09-05 16:25:25 -0700

    Refactored work .bash_profile into pieces and updated ./install

---
+++ bash_it_custom/mac.aliases.bash
@@ -0,0 +1,8 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Prevent `brew doctor` from freaking out over python-configs in ~/.pyenv
+    alias brew='env PATH=${PATH//$(pyenv root)\/shims:/} brew'
+
+    # Custom brew aliases
+    alias bubo='brew update && brew outdated'
+    alias bubc='brew upgrade && brew cleanup'
+fi
---
+++ nas.profile.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Uncomment to debug .bash_profile
+#set -o xtrace
+
+export PYTHON_VERSION='3.5.1'
+
+source ./includes/bashit.bash
