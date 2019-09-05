b032eed1495f4a5000d768cabe799f53ceec4fbc
Dale Davis <dmdavis.net@github.com>
2019-09-05 16:20:22 -0700

    Removed zsh folders and files.

--- fonts/fonts.plugin.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Returns 0 if installed so function may be called with "if"
-# e.g. if is-font-installed foo; then ...
-is-font-installed() {
-    fontname=$1
-    fc-list | grep -i "$fontname" >/dev/null
-}
