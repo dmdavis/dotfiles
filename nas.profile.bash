018b954ce007dd879ef9eed9ce7a410911d9e889
Dale Davis <dmdavis.net@gmail.com>
2019-09-05 16:32:31 -0700

    Fixed ./includes/* sourcing

--- nas.profile.bash
+++ nas.profile.bash
@@ -3,6 +3,10 @@
 # Uncomment to debug .bash_profile
 #set -o xtrace
 
+# Location of .files directory
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 export PYTHON_VERSION='3.5.1'
 
-source ./includes/bashit.bash
+# shellcheck source=$HOME/.files/includes/bashit.bash
+source "$BASEDIR"/includes/bashit.bash
