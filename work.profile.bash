07fe6502e3d3a32fff4772190163acb49e1fec27
Dale Davis <dmdavis.net@gmail.com>
2020-01-15 16:41:29 -0800

    Bumped $PYTHON_VERSION to 3.7.6 in work.profile.bash

--- work.profile.bash
+++ work.profile.bash
@@ -6,7 +6,7 @@
 # Location of .files directory
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export PYTHON_VERSION='3.7.4'
+export PYTHON_VERSION='3.7.6'
 
 # Your place for hosting Git repos. I use this for private repos.
 export GIT_HOSTING='git@github.com'
