5a17d1ab29dd1f8d0156a25f2b06c005841e80a8
Dale Davis <dmdavis.net@gmail.com>
2020-01-27 13:50:23 -0800

    Added GIT_HOSTING to nas.profile.bash

--- nas.profile.bash
+++ nas.profile.bash
@@ -8,5 +8,9 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export PYTHON_VERSION='3.5.1'
 
+# Your place for hosting Git repos. I use this for private repos.
+# TODO: Set this to NAS git
+export GIT_HOSTING='git@github.com'
+
 # shellcheck source=$HOME/.files/includes/bashit.bash
 source "$BASEDIR"/includes/bashit.bash
