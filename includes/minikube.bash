ab69411476afd89f52f10524e4c5029adae5ab6d
Dale Davis <dmdavis.net@gmail.com>
2020-02-19 15:29:12 -0800

    Increased RAM, CPU, and disk size for minikube cluster

--- includes/minikube.bash
+++ includes/minikube.bash
@@ -3,27 +3,34 @@
 # Some stuff from my brief flirtation with minikube
 # https://github.com/kubernetes/minikube
 if [[ $(command -v minikube) != "" ]]; then
+
     export MINIKUBE_VM_DRIVER="virtualbox"
+    export MINIKUBE_VM_CPUS=4
+    export MINIKUBE_VM_RAM="8192mb"     # 8Gb
+    export MINIKUBE_VM_DISK_SIZE="60g"  # 60Gb
 
-    function kcmk() {
-        alias kubectl="minikube kubectl"
-        msg "kubectl set to minikube kubectl'"
-    }
-    function kcrestore() {
-        unalias kubectl
-        msg "kubectl set to $(command -v kubectl)"
+    alias mkstop="minikube stop"
+    alias mkstart="minikube start"
+    alias mkx="minikube delete"
+
+    function mkcc() {
+        echo "Creating minikube cluster with local Docker registry pod"
+        minikube start \
+            --cpus=$MINIKUBE_VM_CPUS \
+            --memory=$MINIKUBE_VM_RAM \
+            --disk-size=$MINIKUBE_VM_DISK_SIZE \
+            --vm-driver=$MINIKUBE_VM_DRIVER \
+            --addons registry \
+            --insecure-registry "10.0.0.0/24"
     }
 
-    function mkstart() {
-        minikube start --vm-driver=$MINIKUBE_VM_DRIVER --insecure-registry=0.0.0.0/0
-        kcmk
-    }
-    function mkstop() {
-        minikube stop
-        kcrestore
-    }
     function mkx() {
         minikube delete
-        kcrestore
     }
+
+    function dkmkreg() {
+        echo "Redirecting Docker for Mac port 5000 to minikube port 5000. Hit CTRL-C to quit."
+        docker run --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
+    }
+
 fi
