c2a114afd5141f92603ed1247da04118e61ee381
Dale Davis <dmdavis.net@gmail.com>
2020-02-20 13:16:02 -0800

    Added kcd, kccf, and kce aliases to bash_it_custom/kubectl.aliases.bash

--- bash_it_custom/kubectl.aliases.bash
+++ bash_it_custom/kubectl.aliases.bash
@@ -4,4 +4,7 @@ if [[ $(command -v kubectl) != "" ]]; then
     alias kcv="kubectl version"
     alias kcl="kubectl logs"
     alias kclf="kubectl logs -f"
+    alias kcd="kubectl describe"
+    alias kccf="kubectl create -f"
+    alias kce="kubectl edit"
 fi
