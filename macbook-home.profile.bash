dbb0fcb5dcf785de87089f8480b8a07553a147ba
Dale Davis <dmdavis.net@gmail.com>
2020-02-23 12:13:50 -0800

    Added macbook-home.profile.bash and bash_it_enables/

---
+++ macbook-home.profile.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Uncomment to debug .bash_profile
+#set -o xtrace
+
+# Location of .files directory
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export PYTHON_VERSION='3.8.1'
+
+# Your place for hosting Git repos. I use this for private repos.
+export GIT_HOSTING='git@github.com'
+
+# shellcheck source=$HOME/.files/includes/bashit.bash
+source "$BASEDIR"/includes/bashit.bash
+# shellcheck source=$HOME/.files/includes/mac.bash
+source "$BASEDIR"/includes/mac.bash
+
+alias llc='ll | lolcat'
+
+export GEM_HOME="$HOME/.gems"
+export GEM_PATH="$HOME/.gems"
+export PATH="$GEM_PATH/bin:$PATH"
