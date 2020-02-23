d40fd23df40d8f734f70869f38f2452490f9e9b3
Dale Davis <dmdavis.net@gmail.com>
2020-02-23 11:29:44 -0800

    Added mkdke_activate to includes/minikube.bash and bumped RAM back to 8Gb

--- includes/minikube.bash
+++ includes/minikube.bash
@@ -6,9 +6,8 @@ if [[ $(command -v minikube) != "" ]]; then
 
     export MINIKUBE_VM_DRIVER="virtualbox"
     export MINIKUBE_CPUS=4
-    export MINIKUBE_MEMORY="7168mb"  # 7Gb
+    export MINIKUBE_MEMORY="8192mb"  # 8Gb
     export MINIKUBE_DISK_SIZE="60g"  # 60Gb
-    export MINIKUBE_INSECURE_REGISTRY="10.0.0.0/24"
 
     function delete_minikube_ip_from_known_hosts() {
         mk_ip="$(minikube ip)"
@@ -19,13 +18,21 @@ if [[ $(command -v minikube) != "" ]]; then
     alias mk="minikube"
     alias mkip="minikube ip"
     alias mkdash="minikube dashboard"
+    alias mkstat="minikube status"
+    alias mkv="minikube version"
+    alias mkdke="minikube -p minikube docker-env"
+
+    function mkdke_activate() {
+        eval "$(minikube -p minikube docker-env)"
+        msg "Docker now configured to use minikube"
+    }
 
     function mkstart() {
         msg "Creating minikube cluster"
-        minikube start
+        minikube start --insecure-registry "10.0.0.0/24"
         msg "Enabling minikube Docker registry on $(minikube ip):5000"
         minikube addons enable registry
-        msg "Run redirect_5000 to enable Docker to push images to minikube Docker registry"
+        mkdke_activate
     }
 
     function mkstop() {
@@ -40,6 +47,8 @@ if [[ $(command -v minikube) != "" ]]; then
         minikube delete
     }
 
+    # If you want to run Docker for Mac and have `docker push` use the registry
+    # running in minikube, run the following function in another terminal window.
     function redirect_5000() {
         msg "Redirecting Docker for Mac port 5000 to minikube port 5000. Hit CTRL-C to quit."
         docker run --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
