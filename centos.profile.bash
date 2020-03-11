9dab1c9bb8054b16532baebc8921b45f1bce50b5
Dale Davis <dmdavis.net@gmail.com>
2020-03-11 14:46:19 -0700

    Added centos.profile.bash

---
+++ centos.profile.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Uncomment to debug .bash_profile
+#set -o xtrace
+
+# Location of .files directory
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export PYTHON_VERSION='3.7.6'
+
+# Your place for hosting Git repos. I use this for private repos.
+export GIT_HOSTING='git@github.com'
+
+# shellcheck source=$HOME/.files/includes/bashit.bash
+source "$BASEDIR"/includes/bashit.bash
