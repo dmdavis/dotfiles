02dca1bdf4944d97ffe710843aa1ac95d0996458
Dale Davis <dmdavis.net@gmail.com>
2021-10-29 11:11:36 -0700

    Updated `git.aliases.bash` with Centos 7 bash completion initialization

--- bash_it_custom/git.aliases.bash
+++ bash_it_custom/git.aliases.bash
@@ -4,8 +4,18 @@
 # https://github.com/sorin-ionescu/prezto/blob/master/modules/git/alias.zsh
 # 1b441e7 on Apr 4, 2018
 
+# bash_it git completion only loads on macOS because there's no reliable path
+# to it on on the various distributions. Try a few known locations here.
+if [[ "$(uname -s)" != 'Linux' ]] ; then
+    # On Centos 7, git completion is lazy-loaded. __git_complete doesn't exist
+    # until "git TAB" is called, so source the completion file if its there.
+    if [[ -r /usr/share/bash-completion/completions/git ]]; then
+        source /usr/share/bash-completion/completions/git
+    fi
+fi
+
 git-branch-current() {
-  git rev-parse --abbrev-ref HEAD
+    git rev-parse --abbrev-ref HEAD
 }
 
 # Git
