ea7f82cd125b2893a3ff6c20f3f640dd67c65a8a
Dale Davis <dmdavis.net@gmail.com>
2022-02-01 09:16:14 -0800

    Updated bashit.bash to allow theme override before script sourcing

--- includes/bashit.bash
+++ includes/bashit.bash
@@ -5,7 +5,7 @@ export BASH_IT_CUSTOM="$HOME/.files/bash_it_custom/"
 # Lock and Load a custom theme file.
 # Leave empty to disable theming.
 # location /.bash_it/themes/
-export BASH_IT_THEME='zork'
+export BASH_IT_THEME="${BASH_IT_THEME:-zork}"
 
 # (Advanced): Change this to the name of your remote repo if you
 # cloned bash-it with a remote other than origin such as `bash-it`.
