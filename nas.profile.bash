19618436a835a3c979fa256f7dd79d6ae54a1c6e
Dale Davis <dmdavis.net@gmail.com>
2022-02-01 09:45:52 -0800

    Updated nas.profile.bash to load better LS_COLORS on Synology DSM 6

--- nas.profile.bash
+++ nas.profile.bash
@@ -6,13 +6,22 @@
 # Location of .files directory
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export PYTHON_VERSION='3.5.1'
+# Python 2.7.18 is still installed on DSM 6, as well.
+export PYTHON_VERSION='3.8.6'
 
 # Your place for hosting Git repos. I use this for private repos.
 # TODO: Set this to NAS git
 export GIT_HOSTING='git@github.com'
 
+export BASH_IT_THEME=90210
 # shellcheck source=./includes/bashit.bash
 source "$BASEDIR"/includes/bashit.bash
 
 export PATH="/usr/local/mariadb10/bin/:$PATH"
+
+# curl https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/lscolors.sh -o ~/.ls-colors.sh
+
+# Set up LS_COLORS if present
+if [[ -r "$HOME/.ls-colors.sh" ]]; then
+    eval "$(dircolors "$HOME"/.ls-colors.sh)"
+fi
