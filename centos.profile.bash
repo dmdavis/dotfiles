73da4dbaf1e5b3f0812f0b54b4fe7fcdc102a362
Dale Davis <dmdavis.net@gmail.com>
2022-02-03 11:14:25 -0800

    Updated scp aliases on centos.profile.bash

--- centos.profile.bash
+++ centos.profile.bash
@@ -68,11 +68,17 @@ alias llo='lso --color'
 ocpsh() {
     ssh -i ~/id_ocp_dev "core@10.87.88.$1"
 }
+
+# Copy k9s to test cluster
 ocpscpk9s() {
     echo "Copying k9s to core@10.87.88.$1:/var/home/core/"
     scp -i ~/id_ocp_dev ~/local/bin/k9s "core@10.87.88.$1:/var/home/core/"
 }
+
+# Copy test yaml to test cluster
 ocpscptestyaml() {
     echo "Copying snat-ext-ping-test-pod.yaml to core@10.87.88.$1:/var/home/core/"
     scp -i ~/id_ocp_dev ~/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests/tests/test-yaml/snat-ext-ping-test-pod.yaml "core@10.87.88.$1:/var/home/core/"
+    scp -i ~/id_ocp_dev ~/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests/tests/test-yaml/port-translation-test.yaml "core@10.87.88.$1:/var/home/core/"
+    scp -i ~/id_ocp_dev ~/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests/tests/test-yaml/port-translation-test-ha.yaml "core@10.87.88.$1:/var/home/core/"
 }
