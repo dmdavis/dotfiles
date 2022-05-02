eac1238e95a7406ecf6b633a58553abd61367164
Dale Davis <dmdavis.net@gmail.com>
2022-05-02 15:43:27 -0700

    Removed ocp* functions and shortcuts from `includes/work.bash`

--- includes/work.bash
+++ includes/work.bash
@@ -20,15 +20,16 @@ alias mftsnci='time make feature-tests-singlenode-ci HOST_REGISTRY=$DOCKER_REPO_
 alias mftdpdkci='time make feature-tests-dpdk-ci HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
 
 export FT_TEST_YAML="$HOME/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests/tests/test-yaml"
-export OCP_USER="${OCP_USER:-core}"
-export OCP_HOME="/var/home"
-export K8S_USER="${K8S_USER:-ubuntu}"
 export K8S_HOME="/home"
 
+# Legacy; not used
+export OCP_USER="${OCP_USER:-core}"
+export OCP_HOME="/var/home"
+
+
 # Override these in local .bashrc
-export OCP_LAB_NETWORK='127.0.0'
+export K8S_USER="${K8S_USER:-ubuntu}"
 export K8S_LAB_NETWORK='127.0.0'
-export OCP_PRIVATE_KEY="$HOME/id"
 export K8S_PRIVATE_KEY="$HOME/id"
 
 # args: private key path, user, ip/host
@@ -37,20 +38,15 @@ __ssh_lab() {
     ssh -i "$1" "$2@$3"
 }
 
-# Shortcut for SSH'ing into ocp-dev cluster nodes
-ocpsh() {
-    __ssh_lab "$OCP_PRIVATE_KEY" "$OCP_USER" "$OCP_LAB_NETWORK.$1"
-}
-
-# Shortcut for SSH'ing into k8s ha cluster nodes in ocp pool
+# Shortcut for SSH'ing into k8s cluster nodes in OpenStack
 k8ssh() {
     __ssh_lab "$K8S_PRIVATE_KEY" "$K8S_USER" "$K8S_LAB_NETWORK.$1"
 }
 
-# Copy k9s to test cluster
-ocpscpk9s() {
-    echo "Copying k9s to $OCP_USER@$OCP_LAB_NETWORK.$1:$OCP_HOME/$OCP_USER/"
-    scp -i "$OCP_PRIVATE_KEY" ~/local/bin/k9s "$OCP_USER@$OCP_LAB_NETWORK.$1:$OCP_HOME/$OCP_USER/"
+# Copy k9s to test cluster (for one's that don't have it installed)
+scpk9s() {
+    echo "Copying k9s to $K8S_USER@$K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/"
+    scp -i "$K8S_PRIVATE_KEY" ~/local/bin/k9s "$K8S_USER@$K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/"
 }
 
 # args: private key path, user, ip/host, home dir
@@ -61,9 +57,6 @@ __scp_test_yaml() {
 }
 
 # Copy test yaml to test cluster
-ocpscptestyaml() {
-    __scp_test_yaml "$OCP_PRIVATE_KEY" "$OCP_USER" "$OCP_LAB_NETWORK.$1" "$OCP_HOME"
-}
 k8sscptestyaml() {
     __scp_test_yaml "$K8S_PRIVATE_KEY" "$K8S_USER" "$K8S_LAB_NETWORK.$1" "$K8S_HOME"
 }
