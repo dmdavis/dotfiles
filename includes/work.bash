e98ad8207d72ea5eda6377159c308bd11689a489
Dale Davis <dmdavis.net@gmail.com>
2022-06-13 09:44:00 -0700

    Refactored k8s vs ocp flags and functions in `includes/work.bash`

--- includes/work.bash
+++ includes/work.bash
@@ -4,33 +4,46 @@
 export DOCKER_REPO_PORT=5000
 export DOCKER_REPO_HOST='127.0.0.1'
 
-# Build aliases
-alias mba='time make build-all HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-alias mda='time make deploy-all HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
+# Feature tests config
+export FT_PATH="$HOME/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests"
+export FT_TEST_YAML="$FT_PATH/tests/test-yaml"
+export LAB_PRIVATE_KEY="$FT_PATH/infra/deployer/ci/keys/ftvm_rsa"
 
-alias mfthacd='time make feature-tests-singleclusterHA-cd HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-#alias mfthadev='time make feature-tests-singlecluster-ha-dev HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT DEPLOYER_CONFIG=/home/daled/k8s.ha.json'
-alias mfthadev='time make feature-tests-singlecluster-ha-dev HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-
-alias mftocpdev='time make feature-tests-ocp-dev HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-alias mftvrrpdev='time make feature-tests-ocp-vrrp-dev HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-alias mftocpcd='time make feature-tests-ocp-cd HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-
-alias mftsnci='time make feature-tests-singlenode-ci HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-alias mftdpdkci='time make feature-tests-dpdk-ci HOST_REGISTRY=$DOCKER_REPO_HOST:$DOCKER_REPO_PORT'
-
-export FT_TEST_YAML="$HOME/go/src/ssd-git.juniper.net/contrail/cn2/feature_tests/tests/test-yaml"
-export K8S_HOME="/home"
-
-# Legacy; not used
+# Lab users and home folder paths for various FT cluster flavors
 export OCP_USER="${OCP_USER:-core}"
-export OCP_HOME="/var/home"
-
-
-# Override these in local .bashrc
+export OCP_HOME="${OCP_HOME:-/var/home}"
+export OCP_NETWORK="10.87.89"
 export K8S_USER="${K8S_USER:-ubuntu}"
-export K8S_LAB_NETWORK='127.0.0'
-export K8S_PRIVATE_KEY="$HOME/id"
+export K8S_HOME="${K8S_HOME:-/home}"
+export K8S_NETWORK="10.87.88"
+
+# Initially default to k8s flavors
+export LAB_USER="${LAB_USER:-ubuntu}"
+export LAB_HOME="${LAB_HOME:-/home}"
+export LAB_NETWORK="${LAB_NETWORK:-10.87.88}"
+
+k8suconf() {
+    echo "LAB_USER        = $LAB_USER"
+    echo "LAB_HOME        = $LAB_HOME"
+    echo "LAB_NETWORK     = $LAB_NETWORK"
+    echo "LAB_PRIVATE_KEY = $LAB_PRIVATE_KEY"
+}
+
+k8socp() {
+    export LAB_USER="$OCP_USER"
+    export LAB_HOME="$OCP_HOME"
+    export LAB_NETWORK="$OCP_NETWORK"
+    echo "k8s helper functions set for OCP clusters"
+    k8suconf
+}
+
+k8sk8s() {
+    export LAB_USER="$K8S_USER"
+    export LAB_HOME="$K8S_HOME"
+    export LAB_NETWORK="$K8S_NETWORK"
+    echo "k8s helper functions set for native k8s clusters"
+    k8suconf
+}
 
 # args: private key path, user, ip/host
 __ssh_lab() {
@@ -40,13 +53,15 @@ __ssh_lab() {
 
 # Shortcut for SSH'ing into k8s cluster nodes in OpenStack
 k8ssh() {
-    __ssh_lab "$K8S_PRIVATE_KEY" "$K8S_USER" "$K8S_LAB_NETWORK.$1"
+    # TODO Add arg check
+    __ssh_lab "$LAB_PRIVATE_KEY" "$LAB_USER" "$LAB_NETWORK.$1"
 }
 
 # Copy k9s to test cluster (for one's that don't have it installed)
 scpk9s() {
-    echo "Copying k9s to $K8S_USER@$K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/"
-    scp -i "$K8S_PRIVATE_KEY" ~/local/bin/k9s "$K8S_USER@$K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/"
+    # TODO Add arg check
+    echo "Copying k9s to $LAB_USER@$LAB_NETWORK.$1:$LAB_HOME/$LAB_USER/"
+    scp -i "$LAB_PRIVATE_KEY" ~/local/bin/k9s "$LAB_USER@$LAB_NETWORK.$1:$LAB_HOME/$LAB_USER/"
 }
 
 # args: private key path, user, ip/host, home dir
@@ -57,13 +72,15 @@ __scp_test_yaml() {
 }
 
 k8sgetconfig() {
-    scp -i "$K8S_PRIVATE_KEY" "$K8S_USER@$K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/.kube/config" "$HOME/$K8S_LAB_NETWORK.$1.kubeconfig"
-    echo "Copied $K8S_LAB_NETWORK.$1:$K8S_HOME/$K8S_USER/.kube/config to $HOME/$K8S_LAB_NETWORK.$1.kubeconfig"
+    # TODO Add arg check
+    scp -i "$LAB_PRIVATE_KEY" "$LAB_USER@$LAB_NETWORK.$1:$LAB_HOME/$LAB_USER/.kube/config" "$HOME/$LAB_NETWORK.$1.kubeconfig"
+    echo "Copied $LAB_NETWORK.$1:$LAB_HOME/$LAB_USER/.kube/config to $HOME/$LAB_NETWORK.$1.kubeconfig"
 }
 
 # Copy test yaml to test cluster
 k8sscptestyaml() {
-    __scp_test_yaml "$K8S_PRIVATE_KEY" "$K8S_USER" "$K8S_LAB_NETWORK.$1" "$K8S_HOME"
+    # TODO Add arg check
+    __scp_test_yaml "$LAB_PRIVATE_KEY" "$LAB_USER" "$LAB_NETWORK.$1" "$LAB_HOME"
 }
 
 # Clean up dirty, bloated build directory.
