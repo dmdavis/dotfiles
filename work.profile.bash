3360bd08a98f0dbf1183522d1dddeda2489d6929
Dale Davis <dmdavis.net@gmail.com>
2022-07-08 18:05:59 -0700

    Added `.iterm2_shell_integration.bash`, `oldtonew`, and `newtoold`

--- work.profile.bash
+++ work.profile.bash
@@ -120,4 +120,21 @@ function prev() {
     sh -c "pet new $(printf %q "$PREV")"
 }
 
+export OLD_MACBOOK=''
+export NEW_MACBOOK=''
+
+alias oldmb='ssh $DEV_USER@$OLD_MACBOOK'
+alias newmb='ssh $DEV_USER@$NEW_MACBOOK'
+
+function oldtonew() {
+    # oldtonew ~/go/src/ssd-git.juniper.net/contrail
+    mkdir -p "$1"
+    rsync -azvhP "${DEV_USER}@${OLD_MACBOOK}:${1}/" "$1"
+}
+
+function newtoold() {
+    ssh "${DEV_USER}@${NEW_MACBOOK}" mkdir -p "$1"
+    rsync -azvhP "$1/" "${DEV_USER}@${OLD_MACBOOK}:${1}"
+}
+
 export PATH="${HOME}/bin:$PATH"
