b2b2cd91a206c3e406d1c247064a19740384a256
Dale Davis <dmdavis.net@gmail.com>
2022-08-04 16:10:57 -0700

    Added `~/go/bin` to PATH in `dev-vms.bash`

--- includes/dev-vms.bash
+++ includes/dev-vms.bash
@@ -131,3 +131,5 @@ function aws() {
 export ARTIFACTORY_HOST=""
 export ARTIFACTORY_URL="https://${ARTIFACTORY_HOST}/artifactory"
 export COMMON_PYPI_REMOTE="${ARTIFACTORY_URL}/api/pypi/pypi-remote/simple"
+
+export PATH=${HOME}/go/bin:${PATH}
