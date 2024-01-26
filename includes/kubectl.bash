de9d9591419548a461e059bf7a243ba8a2ce9d02
Dale Davis <dmdavis.net@gmail.com>
2024-01-25 21:16:20 -0800

    Added kubectl aliases to includes/kubectl.bash

--- includes/kubectl.bash
+++ includes/kubectl.bash
@@ -5,4 +5,13 @@ if [[ "$DOTFILE_TRACE" -eq 1 ]]; then
     echo "PATH = $PATH"
 fi
 
+# Other kubectl alias are defined in Bash-Its `kubectl` alias extension.
+# Enable it via `bashit enable alias kubectl`.
+
+alias kcds='kubectl describe service'
+alias kcgrs='kubectl get replicasets'
+alias kcgs='kubectl get services'
+alias kcgsan='kubectl get services --all-namespaces'
+alias kclp="kubectl label pods"
 alias kcx='kubectl exec -it'
+alias kcxsp="kubectl expose"
