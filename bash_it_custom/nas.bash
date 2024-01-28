be97fcbac94f7f5d4969d9fec214087234b607dc
Dale Davis <dmdavis.net@gmail.com>
2024-01-28 11:54:42 -0800

    Added nuc10 and pi-hole shortcuts to bash_it_custom/nas.bash

--- bash_it_custom/nas.bash
+++ bash_it_custom/nas.bash
@@ -5,7 +5,18 @@ export NAS_IP=''
 export NAS_SSH_PORT=''
 export NAS_USER=''
 
+export NUC_IP=''
+export NUC_SSH_PORT=''
+export NUC_USER=''
+
+export PI_HOLE_IP=''
+export PI_HOLE_SSH_PORT=''
+export PI_HOLE_USER=''
+
 alias nas='ssh -p $NAS_SSH_PORT $NAS_USER@$NAS_IP'
+alias nuc='ssh -p $NUC_SSH_PORT $NUC_USER@$NUC_IP'
+alias pihole='ssh -p $PI_HOLE_SSH_PORT $PI_HOLE_USER@$PI_HOLE_IP'
+alias pi-hole='ssh -p $PI_HOLE_SSH_PORT $PI_HOLE_USER@$PI_HOLE_IP'
 
 # Copy a local file to a remote directory on the NAS
 # Ex: cpnas ~/foo ~/
