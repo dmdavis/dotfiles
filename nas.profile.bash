bcee8ad74ef5e707ce568ed46da3857a642d7838
Dale Davis <dmdavis.net@gmail.com>
2024-03-18 14:45:27 -0700

    Updated nas.profile.bash

--- nas.profile.bash
+++ nas.profile.bash
@@ -6,8 +6,14 @@
 # Location of .files directory
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Python 2.7.18 is still installed on DSM 6, as well.
-export PYTHON_VERSION='3.8.6'
+# Python 2.17.18 is still installed on DSM 7 under /usr/local/bin
+# export PYTHON_VERSION='2.17.18'
+
+# Python 3.8.12 is the default DSM 7 Python
+# export PYTHON_VERSION='3.8.12'
+
+# Python 3.11.5 is installed via SynoCommunity
+export PYTHON_VERSION='3.11.5'
 
 # Your place for hosting Git repos. I use this for private repos.
 # TODO: Set this to NAS git
@@ -19,12 +25,14 @@ source "$BASEDIR"/includes/bashit.bash
 # shellcheck source=./includes/ls-colors.bash
 source "$BASEDIR"/includes/ls-colors.bash
 
-export PATH="$HOME/bin:/usr/local/mariadb10/bin/:$PATH"
+export PATH="$HOME/bin:$PATH"
 export SHELL='/bin/bash'
 
-alias py='python3'
-
-export GITLAB_HOME='/volume1/docker/volumes/gitlab-ce'
+alias python3=python3.11
+alias python=python3.11
+alias py=python3.11
+alias vim=/usr/local/bin/vim
 
 # Local autojump built from source.
 [[ -s /var/services/homes/dale/.autojump/etc/profile.d/autojump.sh ]] && source /var/services/homes/dale/.autojump/etc/profile.d/autojump.sh
+
